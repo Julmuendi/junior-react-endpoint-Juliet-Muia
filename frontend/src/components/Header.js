441b9a4f620994046a9a49ff7b237268342f1fcf
Julmuendi <Julmuia@gmail.com>
2022-06-03 10:36:03 +0300

    installed dependencies, created frontend folder and added the header.js file

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import logo from '../images/logo.png'
+import Vector from '../images/Vector.png'
+import Cart from '../images/Cart.png'
+
+
+export class Header extends Component {
+  render() {
+    return (
+      <div className='header'>
+        <div className='headerul'>
+          
+              <div><a href=''>Women</a></div>
+              <div><a href=''>Men</a></div>
+              <div><a href=''>Kids</a></div>
+          
+        </div>
+         <img src={logo} className='logo' alt='logo'></img>
+         <div className='cartnprice'>
+           <div className='pricenvector'>
+              <div>$ <img className='vector' src={Vector} alt='vector'/>
+              </div>
+            </div>
+              <div><img src={Cart} className='cart'/></div>
+         </div>
+      </div>
+    )
+  }
+}
+
+export default Header
---
+++ frontend/src/components/Products.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Products extends Component {
+  render() {
+    return (
+      <div className='categoryname'>
+        <h2>Category name</h2>
+      </div>
+    )
+  }
+}
+
+export default Products
